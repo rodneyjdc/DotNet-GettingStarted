f2d0c936451edb3d8e12e0e06091baa808a9e1a1
Rodney Canlas <canlasrj@gmail.com>
2021-03-29 08:59:22 -0700

    Added aGet action that returns a list of the query names and their values.

--- Controllers/HelloController.cs
+++ Controllers/HelloController.cs
@@ -7,8 +7,19 @@ namespace dotNetStarter.Controllers
     public class HelloController : ControllerBase
     {
         [HttpGet]
-        public IActionResult Get() {
-            return Ok("Hello from .NET Core!");
+        [Route("headerInfo/")]
+        public IActionResult GetHeaders() {
+            // return Ok("Hello from .NET Core!");
+            base.Response.Cookies.Append("message", "This message is a cookie!");
+            var headers = base.Request.Headers;
+            return base.Ok(headers);
+        }
+
+        [HttpGet]
+        [Route("queryInfo/")]
+        public IActionResult GetQueries() {
+            var queries = base.Request.QueryString;
+            return base.Ok(queries);
         }
     }
 }
